1d65df42c3057211196d1a67c128df7bd4e82ce7
Kiara Kidd <148988970+KiddKiara@users.noreply.github.com>
2024-08-05 21:43:19 -0400

    Implement remove_adjacent and linear_merge functions

--- list2.py
+++ list2.py
@@ -6,8 +6,13 @@
 # modify the passed in list.
 
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+    if not nums:
+        return []
+    result = [nums[0]]
+    for num in nums[1:]:
+        if num != result[-1]:
+            result.append(num)
+    return result
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -16,46 +21,5 @@ def remove_adjacent(nums):
 # pass of both lists.
 
 def linear_merge(list1, list2):
-    # +++your code here+++
-    return
-
-
-# Note: the solution above is kind of cute, but unforunately list.pop(0)
-# is not constant time with the standard python list implementation, so
-# the above is not strictly linear time.
-# An alternate approach uses pop(-1) to remove the endmost elements
-# from each list, building a solution list which is backwards.
-# Then use reversed() to put the result back in the correct order. That
-# solution works in linear time, but is more ugly.
-
-
-# Simple provided test() function used in main() to print
-# what each function returns vs. what it's supposed to return.
-
-def test(got, expected):
-    if got == expected:
-        prefix = ' OK '
-    else:
-        prefix = '  X '
-    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
-
-
-# Calls the above functions with interesting inputs.
-def main():
-    print('remove_adjacent')
-    test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
-    test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
-    test(remove_adjacent([]), [])
-
-    print()
-    print('linear_merge')
-    test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
-         ['aa', 'bb', 'cc', 'xx', 'zz'])
-    test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
-         ['aa', 'bb', 'cc', 'xx', 'zz'])
-    test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
-         ['aa', 'aa', 'aa', 'bb', 'bb'])
-
-
-if __name__ == '__main__':
-    main()
+    result = []
+    i, j = 0, 
